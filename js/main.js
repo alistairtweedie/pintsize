6f34df44a452f0dd427345d820b29eeb73a10e02
alistairtweedie <info@alistairtweedie.com>
2015-03-05 21:31:20 +0000

    Project commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*-------------------------------------------------------*\
+    Main
+/*-------------------------------------------------------*/
+window.console && window.console.info("Like what you see? This is still in development.");
+
+
+$(document).ready(function() {
+
+
+});
