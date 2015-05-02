99af206bf3133ec2a250515ee4d0e0f5d8899640
Falko Joseph <falko@typework.com>
2015-05-02 20:25:23 +0200

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,12 @@
 var gulp = require('gulp'),
     fs = require('fs'),
     shell = require('gulp-shell'),
+    rm = require( 'gulp-rm' ),
+    filter = require('gulp-filter'),
     es6transpiler = require('gulp-es6-transpiler'),
+    ext_replace = require('gulp-ext-replace'),
     clean = require('gulp-clean'),
+    changed = require('gulp-changed'),
     concat = require('gulp-concat'),
     swig = require('gulp-swig'),
     watch = require('gulp-watch'),
@@ -21,6 +25,7 @@ var gulp = require('gulp'),
     postcssNested = require('postcss-nested'),
     lost = require('lost'),
     zIndex = require('postcss-zindex'),
+    watch = require('gulp-watch'),
     simpleExtend = require('postcss-simple-extend');
 
 // PostCSS
@@ -41,18 +46,43 @@ var processors = [
   lost
 ];
 
+// Cleanup
+gulp.task('clean:images', function(cb) {
+  return gulp.src(['dist/images/**/*.*'], {read: false})
+    .pipe(clean({force: true}));
+});
+
+gulp.task('clean:fonts', function(cb) {
+  return gulp.src(['dist/fonts/**/*.*'], {read: false})
+    .pipe(clean({force: true}));
+});
+
+gulp.task('clean:css', function(cb) {
+  return gulp.src(['dist/css/**/*.*'], {read: false})
+    .pipe(clean({force: true}));
+});
+
+gulp.task('clean:js', function(cb) {
+  console.log('clean EVENTTTTT!!!');
+  return gulp.src(['dist/js/**/*.*'], {read: false})
+    .pipe(clean({force: true}));
+});
+
+gulp.task('clean:html', function(cb) {
+  return gulp.src(['dist/**/*.html'], {read: false})
+    .pipe(clean({force: true}));
+});
+
 // Pre process
-gulp.task('pre-process', function(){
+gulp.task('pre-process', ['clean:css'], function(){
   var input = fs.readFileSync('./src/css/base.css', 'utf8');
 
-  gulp.src('./src/css/**/*.css')
-      .pipe(watch(function(files) {
-        gulp.src('./src/css/base.css')
-          .pipe(postcss(processors))
-          .pipe(cssnext(input))
-          .pipe(gulp.dest('./dist/css/'))
-          .pipe(browserSync.reload({stream:true}));
-      }));
+  gulp.src('src/css/base.css')
+      .pipe(watch('src/css/**/*.css'))
+      .pipe(postcss(processors))
+      .pipe(cssnext(input))
+      .pipe(gulp.dest('./dist/css/'))
+      .pipe(browserSync.reload({stream:true}));
 });
 
 // Initialize browser-sync
@@ -66,47 +96,42 @@ gulp.task('browser-sync', function() {
 });
 
 // Javascript ES6 transpiler
-gulp.task('es6', function () {
-  return gulp.src('./src/es6/**/*.es6')
-      .pipe(watch(function(files) {
-        return files.pipe(es6transpiler())
-          .pipe(concat('main.js'))
-          .pipe(gulp.dest('./dist/js/'))
-          .pipe(browserSync.reload({stream:true}));
-      }));
+gulp.task('es6', ['clean:js'], function () {
+  return gulp.src('src/es6/**/*.es6')
+      .pipe(watch('src/es6/**/*.es6'))
+      .pipe(es6transpiler())
+      .pipe(ext_replace('.js'))
+      .pipe(gulp.dest('./dist/js/'))
+      .pipe(browserSync.reload({stream:true}));
 });
 
 // Swig
-gulp.task('templates', function() {
-  gulp.src(['./src/**/*.html'])
-    .pipe(watch())
+gulp.task('templates', ['clean:html'], function() {
+  gulp.src(['src/**/*.html'])
+    .pipe(watch('src/**/*.html'))
     .pipe(shell('gulp build'))
-    .pipe(shell('gulp clean'))
     .pipe(browserSync.reload({stream:true}));
 });
 
 // Images
-gulp.task('images', function () {
-  return gulp.src(['./src/images/**/*.*'])
-    .pipe(watch())
-    .pipe(gulp.dest('./dist/images'));
+gulp.task('images', ['clean:images'], function () {
+  gulp.src(['src/images/**/*.*'])
+    .pipe(watch('src/images/**/*.*'))
+    .pipe(gulp.dest('./dist/images'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 // Fonts
-gulp.task('fonts', function () {
-  return gulp.src(['./src/fonts/**/*.*'])
-    .pipe(watch())
-    .pipe(gulp.dest('./dist/fonts'));
-});
-
-// Clean
-gulp.task('clean', function () {
-  return gulp.src(['./dist/_includes/'], {read: false})
-    .pipe(clean());
+gulp.task('fonts', ['clean:fonts'], function () {
+  return gulp.src(['src/fonts/**/*.*'])
+    .pipe(watch('src/fonts/**/*.*'))
+    .pipe(shell('gulp clean:fonts'))
+    .pipe(gulp.dest('./dist/fonts'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 // Build
-gulp.task('build', function() {
+gulp.task('build', ['clean:html'], function() {
   gulp.src(['./src/**/*.html'])
     .pipe(swig({defaults: { cache: false }}))
     .pipe(gulp.dest('./dist'));
@@ -118,11 +143,6 @@ gulp.task('bs-reload', function () {
 });
 
 // Default task
-gulp.task('default', ['templates', 'images', 'fonts', 'pre-process', 'es6', 'bs-reload', 'browser-sync'], function(){
+gulp.task('default', ['templates', 'pre-process', 'es6', 'images', 'fonts', 'bs-reload', 'browser-sync'], function(){
   gulp.start('pre-process');
-  gulp.watch('src/css/**/*.css', ['pre-process']);
-  gulp.watch('src/es6/**/*.es6', ['es6']);
-  gulp.watch('dist/css/base.css', ['bs-reload']);
-  gulp.watch('src/images/**/*.*').on('change', ['images']);
-  gulp.watch('src/fonts/**/*.*').on('change', ['fonts']);
 });
